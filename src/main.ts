364a8b001b43068ceb1292d8d8347ab9a7ba934f
Ahmet <dede_dede_ahmet@hotmail.com>
2023-07-05 23:27:22 +0300

    Pinia using persisted state plugin

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import 'bootstrap/dist/css/bootstrap.css'
 
 import App from './App.vue'
 
 const app = createApp(App)
 const pinia = createPinia()
+
+pinia.use(piniaPluginPersistedstate)
 
 app.use(pinia)
 app.mount('#app')
