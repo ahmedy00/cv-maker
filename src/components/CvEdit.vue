067f81f92777de7b13cfbaa8ec0d11bc9f0a5d8a
Ahmet <dede_dede_ahmet@hotmail.com>
2023-07-05 23:10:42 +0300

    CvEdit component defined

---
+++ src/components/CvEdit.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="edit-wrapper m-auto">
+    <div class="edit-header"/>
+    <Image/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Image from '@/components/Image.vue'
+
+
+</script>
+
+<style scoped>
+
+.edit-wrapper {
+  max-width: 210mm;
+  height: 297mm;
+  border: 1px solid black;
+  margin-top: 1rem;
+}
+
+.edit-header {
+  height: 5rem;
+  /* TODO: Should be user defined */
+  background-color: #f0f0f0;
+  /* TODO: Should be user defined */
+  border-bottom: 5px solid #e0e0e0;
+}
+
+</style>
