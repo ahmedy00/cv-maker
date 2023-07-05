9d3dda73f855b760d27ff5061f77ff075beca524
Ahmet <dede_dede_ahmet@hotmail.com>
2023-07-05 23:10:57 +0300

    Added image import functionality

---
+++ src/components/Image.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="edit-photo d-flex justify-content-center">
+    <img v-if="!selectedImage" :src="NoProfile" alt="">
+    <img v-else :src="selectedImage" alt="">
+    <input type="file" id="img-file" name="img-file" accept="image/*" @change="uploadImage">
+  </div>
+</template>
+
+<script setup lang="ts">
+import NoProfile from '@/assets/noprofile.jpg'
+import { onBeforeMount, ref } from 'vue'
+
+const selectedImage = ref()
+
+const uploadImage = (event) => {
+  selectedImage.value = URL.createObjectURL(event.target.files[0])
+  localStorage.setItem('userPhoto', JSON.stringify(selectedImage.value))
+}
+
+onBeforeMount(() => {
+  const photo = JSON.parse(localStorage.getItem('userPhoto'))
+  if (photo) {
+    selectedImage.value = JSON.parse(localStorage.getItem('userPhoto'))
+  }
+})
+
+</script>
+
+<style scoped>
+
+.edit-photo {
+  cursor: pointer;
+}
+
+.edit-photo img {
+  max-width: 125px;
+  border-radius: 12px;
+  position: absolute;
+  top: 3rem;
+}
+
+.edit-photo input[type = "file"] {
+  position: absolute;
+  top: 3rem;
+  width: 125px;
+  height: 125px;
+  opacity: 0;
+  cursor: pointer;
+}
+
+/* breakpoints */
+
+@media only screen and (max-width: 768px) {
+  .edit-photo img {
+    max-width: 75px;
+  }
+
+  .edit-photo input[type = "file"] {
+    width: 75px;
+    height: 75px;
+  }
+}
+
+</style>
