df349196baacf27046f33ce8c4dccfb5ffafe938
Ahmet <dede_dede_ahmet@hotmail.com>
2023-07-05 21:52:51 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    'prettier'
+  ],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'object-curly-spacing': ['error', 'always'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'prefer-const': ['error', {
+      destructuring: 'all',
+      ignoreReadBeforeAssign: true,
+    }]
+  },
+}
---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>CV</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
