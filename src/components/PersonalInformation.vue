1dff5312ad9859b968d5fb188fe27ad9352f87ed
Ahmet <dede_dede_ahmet@hotmail.com>
2023-07-10 20:39:41 +0300

    Created personal information template, will be functional in next commit for this component

---
+++ src/components/PersonalInformation.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="edit-personal-information">
+    <div class="container g-0 h-100 d-flex flex-row">
+      <div class="info row g-0 w-50 d-flex flex-column">
+        <div class="col full-name">AHMET DEDE</div>
+        <div class="col title">SOFTWARE DEVELOPER</div>
+        <div class="col mail">dede_dede_ahmet@hotmail.com</div>
+        <div class="col phone-number"></div>
+      </div>
+      <div class="social row g-0 w-50 d-flex flex-column">
+        <div class="col ">test</div>
+        <div class="col ">test</div>
+        <div class="col ">test</div>
+        <div class="col ">test</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+.edit-personal-information {
+  width: calc(100% - 125px);
+  height: 100%;
+  float: right;
+}
+
+</style>
