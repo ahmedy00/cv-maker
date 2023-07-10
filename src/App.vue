b87767365e6bc59727e830372fed04f24705f060
Ahmet <dede_dede_ahmet@hotmail.com>
2023-07-10 20:36:41 +0300

    Added padding to app wrapper and random font

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
-  <CvEdit/>
+  <div class="app-wrapper">
+    <CvEdit/>
+  </div>
 </template>
 
 <script setup lang="ts">
 import CvEdit from '@/components/CvEdit.vue'
 
 </script>
+
+<style>
+  * {
+    font-family: "Lucida Console", "Courier New", monospace;;
+  }
+
+  .app-wrapper {
+    padding: 2rem 1rem 2rem 1rem;
+  }
+</style>
