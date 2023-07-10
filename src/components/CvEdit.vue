bd7e0ca8df7268b748ad4605af007f4cbe7a9c86
Ahmet <dede_dede_ahmet@hotmail.com>
2023-07-10 20:37:18 +0300

    Added PersonalInformation and Experience components and made style changes

--- src/components/CvEdit.vue
+++ src/components/CvEdit.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="edit-wrapper m-auto">
-    <div class="edit-header"/>
-    <Image/>
+    <div class="edit-header">
+      <Image/>
+      <PersonalInformation/>
+    </div>
+    <div class="edit-body pt-2 px-4">
+      <Experience/>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import Image from '@/components/Image.vue'
+import PersonalInformation from '@/components/PersonalInformation.vue'
+import Experience from '@/components/Experience.vue'
 
 
 </script>
@@ -16,16 +23,17 @@ import Image from '@/components/Image.vue'
 .edit-wrapper {
   max-width: 210mm;
   height: 297mm;
-  border: 1px solid black;
-  margin-top: 1rem;
+  box-shadow: 0 11px 15px -7px rgba(0, 0, 0, .2),0 24px 38px 3px rgba(0, 0, 0, .2),0 9px 46px 8px rgba(0, 0, 0, .2)
 }
 
 .edit-header {
-  height: 5rem;
+  height: 125px;
   /* TODO: Should be user defined */
-  background-color: #f0f0f0;
-  /* TODO: Should be user defined */
-  border-bottom: 5px solid #e0e0e0;
+  background-color: #42b883;
+}
+
+.edit-body {
+
 }
 
 </style>
