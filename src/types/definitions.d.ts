a1647bb0a97a161d5a9a9c43c37471d8ee1d99d2
Ahmet <dede_dede_ahmet@hotmail.com>
2023-07-12 06:45:24 +0300

    Created a .d.ts file and included type definitions

---
+++ src/types/definitions.d.ts
@@ -0,0 +1,9 @@
+declare module 'definitions' {
+    // Imports
+    import Experience from '@/types/store/experience/experience'
+    import State from '@/types/store/state'
+
+    // Exports
+    export type ExperienceType = Experience
+    export type StateType = State
+}
