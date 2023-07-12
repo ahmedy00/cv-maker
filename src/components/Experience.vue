e2d0b7621927f5f4a1a388bece13f86e9cd845ec
Ahmet <dede_dede_ahmet@hotmail.com>
2023-07-12 06:43:15 +0300

    -Animations are deleted -Template information coming reactively

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -3,41 +3,86 @@
     <div style="border-bottom: 4px solid #42b883">
       <h4 class="w-50">Work Experience</h4>
     </div>
-    <div class="mt-2 p-1" style="border: 1px dashed black; border-radius: 7px">
-      <div>Software Engineer</div>
-      <div style="color: #42b883">Company Name</div>
-      <div class="d-flex flex-row">
-        <span class="me-3">02/2022-Current</span>
-        <span>Ankara/Turkey</span>
+    <div v-if="!!experienceList.length" class="mt-2 p-1">
+      <div v-for="(experience, index) in experienceList" :key="`${index}-experience`">
+        <div class="float-end">
+          <button @click="removeExperience(index)" class="btn border-0">
+            <i style="color: brown; font-size: 1.2rem" class="fa fa-trash me-3" aria-hidden="true"></i>
+          </button>
+          <button @click="editExperience(experience, index)" class="btn border-0">
+            <i style="color: coral; font-size: 1.2rem" class="fa fa-pencil" aria-hidden="true"></i>
+          </button>
+        </div>
+        <div>{{ capitalize(experience.role) }}</div>
+        <div style="color: #42b883">{{ capitalize(experience.companyName) }}</div>
+        <div class="d-flex flex-row">
+          <span class="me-3">{{ experience.startDateMonth + '/' + experience.startDateYear }} - {{ experience.endDateMonth ? (experience.endDateMonth + '/' + experience.endDateYear) : 'Current' }}</span>
+          <span>{{ capitalize(experience.city) + '/' + capitalize(experience.country) }}</span>
+        </div>
+        <ul class="list-group-flush">
+          <li v-for="(detail, index) in experience.details" :key="`${index}-detail`">{{ detail }}</li>
+        </ul>
       </div>
-      <ul class="list-group-flush">
-        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, ut.</li>
-        <li>Lorem ipsum dolor sit amet.</li>
-        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut distinctio itaque odit.</li>
-      </ul>
     </div>
     <button class="add-experience-button" @click="openModal">
       <i class="fa fa-plus" aria-hidden="true"></i>
     </button>
   </div>
-  <transition name="slide-center">
-    <Modal v-if="isModalVisible" @close-modal="closeModal"/>
-  </transition>
+  <div>
+    <Modal v-if="isModalVisible" @close-modal="closeModal">
+      <ExperienceForm
+          :editable-experience="editableExperience"
+          :is-experience-editing="isExperienceEditing"
+          :experience-index="experienceIndex"
+          @close-modal="closeModal"/>
+    </Modal>
+  </div>
+
 </template>
 
 <script setup lang="ts">
 import Modal from '@/components/helper/Modal.vue'
-import { ref } from 'vue'
+import { onBeforeMount, ref } from 'vue'
+import ExperienceForm from '@/components/form/ExperienceForm.vue'
+import { useAppStore } from '@/store'
+import { ExperienceType } from 'definitions'
+
+const store = useAppStore()
 
 const isModalVisible = ref(false)
-
 const openModal = () => {
   isModalVisible.value = true
 }
-
 const closeModal = () => {
   isModalVisible.value = false
 }
+
+const experienceList = ref() as ExperienceType[]
+
+const editableExperience = ref()
+
+const isExperienceEditing = ref(false)
+
+const experienceIndex = ref()
+
+const capitalize = (word: string) => {
+  return word.split(' ').map((w) => w.charAt(0).toUpperCase() + w.slice(1)).join('')
+}
+
+const removeExperience = (index: number) => {
+  experienceList.value.splice(index, 1)
+}
+
+const editExperience = (experience: ExperienceType, index: number) => {
+  editableExperience.value = experience
+  experienceIndex.value = index
+  isExperienceEditing.value = true
+  isModalVisible.value = true
+}
+
+onBeforeMount(() => {
+  experienceList.value = store.workExperience
+})
 
 </script>
 
@@ -54,70 +99,6 @@ const closeModal = () => {
 .add-experience-button i {
   font-size: 2.5rem;
   color: #42b883
-}
-
-/* ANIMATIONS */
-
-.slide-center-enter-active {
-	-webkit-animation: slide-fwd-center 0.45s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
-  animation: slide-fwd-center 2s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
-}
-
-.slide-center-leave-active {
-	-webkit-animation: slide-bck-center .45s cubic-bezier(.47,0.000,.745,.715) both;
-  animation:slide-bck-center 2s cubic-bezier(.47,0.000,.745,.715) both
-}
-
-/*
- * ----------------------------------------
- * animation slide-fwd-center
- * ----------------------------------------
- */
-@-webkit-keyframes slide-fwd-center {
-  0% {
-    -webkit-transform: translateZ(0);
-    transform: translateZ(0);
-  }
-  100% {
-    -webkit-transform: translateZ(160px);
-    transform: translateZ(160px);
-  }
-}
-@keyframes slide-fwd-center {
-  0% {
-    -webkit-transform: translateZ(0);
-    transform: translateZ(0);
-  }
-  100% {
-    -webkit-transform: translateZ(160px);
-    transform: translateZ(160px);
-  }
-}
-
-/*
- * ----------------------------------------
- * animation slide-bck-center
- * ----------------------------------------
- */
-@-webkit-keyframes slide-bck-center {
-  0% {
-    -webkit-transform: translateZ(0);
-    transform: translateZ(0);
-  }
-  100% {
-    -webkit-transform: translateZ(-400px);
-    transform: translateZ(-400px);
-  }
-}
-@keyframes slide-bck-center {
-  0% {
-    -webkit-transform: translateZ(0);
-    transform: translateZ(0);
-  }
-  100% {
-    -webkit-transform: translateZ(-400px);
-    transform: translateZ(-400px);
-  }
 }
 
 </style>
