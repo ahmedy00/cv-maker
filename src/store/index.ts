0b82cdfb1fbcab684fb677b52529994d060287bd
Ahmet <dede_dede_ahmet@hotmail.com>
2023-07-12 06:40:27 +0300

    Defined actions to pinia store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,21 @@
 import { defineStore } from 'pinia'
+import { StateType, ExperienceType } from 'definitions'
 
 export const useAppStore = defineStore('app', {
     state: () => ({
-        userPhoto: ''
-    }),
+        userPhoto: '',
+        workExperience: []
+    }) as StateType,
     persist: true,
     actions: {
       setUserPhoto (photo: string) {
           this.userPhoto = photo
+      },
+      setWorkExperience (workExperience: ExperienceType) {
+        this.workExperience.push(workExperience)
+      },
+      updateWorkExperience (workExperience: ExperienceType, index: number) {
+          this.workExperience[index] = workExperience
       }
     }
 
