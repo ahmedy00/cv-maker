767c47709ebe7ca9ce2a4d9576c921748485639e
Ahmet <dede_dede_ahmet@hotmail.com>
2023-07-12 06:39:55 +0300

    Created a reusable button component

---
+++ src/components/helper/Button.vue
@@ -0,0 +1,49 @@
+<template>
+  <button
+      class="btn btn-danger float-start"
+      :class="cancelButtonClasses"
+      @click.prevent="cancelAction"
+      ref="closeRef"
+  >
+    {{ cancelButtonText }}
+  </button>
+  <button
+      class="btn float-end"
+      :class="applyButtonClasses"
+      style="background:#42b883; color: white"
+      @click="applyAction">
+    {{ applyButtonText }}
+  </button>
+</template>
+
+<script setup lang="ts">
+
+defineProps({
+  cancelButtonText: {
+    type: String,
+    default: 'Cancel'
+  },
+  applyButtonText: {
+    type: String,
+    default: 'Save'
+  },
+  cancelButtonClasses: {
+    type: String,
+    default: ''
+  },
+  applyButtonClasses: {
+    type: String,
+    default: ''
+  }
+})
+const emit = defineEmits(['action'])
+
+
+const cancelAction = () => {
+  emit('cancelAction')
+}
+
+const applyAction = () => {
+  emit('applyAction')
+}
+</script>
