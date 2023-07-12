5dffc372fbe1aa91adcf3e0d01a7653cd699fa06
Ahmet <dede_dede_ahmet@hotmail.com>
2023-07-12 06:44:50 +0300

    Work experience type is defined and exported

---
+++ src/types/store/experience/experience.ts
@@ -0,0 +1,14 @@
+type Experience = {
+    city: string,
+    companyName: string,
+    country: string,
+    details: string[],
+    endDateMonth: number
+    endDateYear: number,
+    isStillWorking: boolean,
+    role: string,
+    startDateMonth: number,
+    startDateYear: number
+}
+
+export default Experience
