78a7a1e0a6583c3312e0a376f7158d0248716e3d
Ahmet <dede_dede_ahmet@hotmail.com>
2023-07-13 23:19:32 +0300

    Added a home page component

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home-page">
+    <HomeHeader :header-height="headerHeight"/>
+    <HomeBody :header-height="headerHeight"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import HomeHeader from '@/components/home/HomeHeader.vue'
+import HomeBody from '@/components/home/HomeBody.vue'
+
+const headerHeight = 65
+</script>
+
+
+<style scoped>
+
+.home-page {
+  width: 100%;
+  height: 100%;
+}
+
+</style>
