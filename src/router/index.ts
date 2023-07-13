4e48c6a8911de34b9f81840a32e13022b45b5722
Ahmet <dede_dede_ahmet@hotmail.com>
2023-07-13 23:18:31 +0300

    Adjusted router settings

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import CvEdit from '@/components/CvEdit.vue'
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: HomeView
+        },
+        {
+            path: '/edit',
+            name: 'Edit',
+            component: CvEdit
+        }
+    ]
+})
+
+export default router
