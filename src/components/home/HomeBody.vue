a51016fc88404739edaf0b4763d7108192f8df8d
Ahmet <dede_dede_ahmet@hotmail.com>
2023-07-13 23:22:48 +0300

    Created header and body components for the homepage

---
+++ src/components/home/HomeBody.vue
@@ -0,0 +1,62 @@
+<template>
+  <div :style="{height: `calc(100vh - ${props.headerHeight}px)`}">
+    <section class="d-flex flex-column align-items-center justify-content-center h-50">
+      <h1 class="section-title text-center mb-5">Welcome to the CV-MAKER</h1>
+      <p class="section-info text-center">An approachable, performant and versatile framework for building web user interfaces.</p>
+      <div>
+        <RouterLink to="edit">
+          <button class="btn border-0" style="color: red; background: green">CREATE A CV</button>
+        </RouterLink>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  headerHeight: {
+    type: Number,
+    default: 65
+  }
+})
+
+</script>
+
+
+<style scoped>
+
+.section-title {
+  font-size: 4rem;
+  background: -webkit-linear-gradient(315deg,#42d392 25%,#647eff);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.section-info {
+  font-size: 1.8rem;
+  color: #3c3c3cb3;
+  padding: 0 10px;
+}
+
+@media (max-width: 992px) {
+  .section-title {
+    font-size: 3rem;
+  }
+
+  .section-info {
+    font-size: 1.5rem;
+  }
+}
+
+@media (max-width: 576px) {
+  .section-title {
+    font-size: 2rem;
+  }
+
+  .section-info {
+    font-size: 1rem;
+  }
+}
+
+</style>
---
+++ src/components/home/HomeHeader.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <header :style="{height: `${props.headerHeight}px`, background: '#42b883'}">
+        <div class="container-fluid h-100">
+          <div class="row w-100 h-100">
+            <div class="col-5 ms-5 d-flex align-items-center">
+              <img width="45" src="https://s3-alpha.figma.com/hub/file/1913095808/a7bdc469-cd70-4ea1-bb57-b59204ad8182-cover.png" alt="">
+            </div>
+            <div class="item-container col-6 d-flex flex-row-reverse align-items-center">
+              <span class="item item-link cursor-pointer">Lorem ipsum</span>
+              <span class="item item-link cursor-pointer me-4">Lorem</span>
+              <RouterLink class="item-link" to="">
+                <span class="item me-4">Previous Resumes</span>
+              </RouterLink>
+              <span class="me-4">
+                <a class="item-link item" href="https://github.com/ahmedy00/cv-maker" target="_blank">Contribution</a>
+              </span>
+            </div>
+          </div>
+        </div>
+    </header>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  headerHeight: {
+    type: Number,
+    default: 65
+  }
+})
+
+</script>
+
+
+<style scoped>
+
+.item-link {
+  color: white;
+  text-decoration: none;
+}
+
+.item-container {
+  white-space: nowrap;
+  overflow: hidden;
+}
+
+.item:hover {
+  color: #647eff;
+}
+
+.cursor-pointer {
+  cursor: pointer;
+}
+
+.icon {
+  display: none;
+}
+
+/* breakpoint: 992 */
+
+@media (max-width: 992px) {
+  .item {
+    display: none;
+  }
+
+  .icon {
+    display: block;
+  }
+}
+
+
+</style>
