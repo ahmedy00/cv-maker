60c606a60956daafdb3ddfe14e6b37e0c437ed72
Ahmet <dede_dede_ahmet@hotmail.com>
2023-07-13 23:18:52 +0300

    vue-router using in project

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import 'bootstrap/dist/css/bootstrap.css'
+import router from './router/index'
 
 import App from './App.vue'
 
 const app = createApp(App)
 const pinia = createPinia()
 
+
 pinia.use(piniaPluginPersistedstate)
 
 app.use(pinia)
+app.use(router)
 app.mount('#app')
