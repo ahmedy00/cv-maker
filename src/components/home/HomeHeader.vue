3e0b22285095142a0d89c94836281b6aea5a65c5
Ahmet <dede_dede_ahmet@hotmail.com>
2023-07-18 21:57:55 +0300

    Created a button for language selection

--- src/components/home/HomeHeader.vue
+++ src/components/home/HomeHeader.vue
@@ -3,11 +3,13 @@
     <header :style="{height: `${props.headerHeight}px`, background: '#42b883'}">
         <div class="container-fluid h-100">
           <div class="row w-100 h-100">
-            <div class="col-5 ms-5 d-flex align-items-center">
+            <div class="col-5 ms-5 d-flex align-items-center cursor-pointer" @click="routeToHome">
               <img width="45" src="https://s3-alpha.figma.com/hub/file/1913095808/a7bdc469-cd70-4ea1-bb57-b59204ad8182-cover.png" alt="">
             </div>
             <div class="item-container col-6 d-flex flex-row-reverse align-items-center">
-              <span class="item item-link cursor-pointer">Lorem ipsum</span>
+              <button class="btn item item-button">
+                <i class="fa-solid fa-earth-asia"></i>
+              </button>
               <span class="item item-link cursor-pointer me-4">Lorem</span>
               <RouterLink class="item-link" to="">
                 <span class="item me-4">Previous Resumes</span>
@@ -23,6 +25,8 @@
 </template>
 
 <script setup lang="ts">
+import { languages } from '../../app'
+import router from '../../router'
 
 const props = defineProps({
   headerHeight: {
@@ -31,19 +35,35 @@ const props = defineProps({
   }
 })
 
+const routeToHome = () => {
+  router.push({
+    name: 'Home'
+  })
+}
+
 </script>
 
 
 <style scoped>
+
+.item-container {
+  white-space: nowrap;
+  overflow: hidden;
+}
+
+.item i {
+  font-size: 1.2rem;
+}
 
 .item-link {
   color: white;
   text-decoration: none;
 }
 
-.item-container {
-  white-space: nowrap;
-  overflow: hidden;
+.item-button {
+  font-size: 1.2rem;
+  border: none;
+  color: #fff
 }
 
 .item:hover {
@@ -54,19 +74,12 @@ const props = defineProps({
   cursor: pointer;
 }
 
-.icon {
-  display: none;
-}
 
 /* breakpoint: 992 */
 
 @media (max-width: 992px) {
   .item {
     display: none;
-  }
-
-  .icon {
-    display: block;
   }
 }
 
