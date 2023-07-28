826e1e5e3c88909709b38e559ebd34049174df96
Ahmet <dede_dede_ahmet@hotmail.com>
2023-07-29 00:36:07 +0300

    Added new field to state type

--- src/types/store/state.ts
+++ src/types/store/state.ts
@@ -1,7 +1,10 @@
 import Experience from './experience/experience'
+import Language from './language'
+
 type State = {
     userPhoto: string,
-    workExperience: Experience[]
+    workExperience: Experience[],
+    currentLanguage: Language
 }
 
 export default State
