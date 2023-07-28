622e4dce159e160e1cb1d195865134bcde38c43c
Ahmet <dede_dede_ahmet@hotmail.com>
2023-07-29 00:35:43 +0300

    PrimeVue removed and Vuetify imported

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,19 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import 'bootstrap/dist/css/bootstrap.css'
 import router from './router/index'
 import { setupI18n } from './locales/i18n'
-import PrimeVue from 'primevue/config'
 
 import App from './App.vue'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 
 const app = createApp(App)
@@ -18,5 +28,5 @@ pinia.use(piniaPluginPersistedstate)
 setupI18n(app)
 app.use(pinia)
 app.use(router)
-app.use(PrimeVue)
+app.use(vuetify)
 app.mount('#app')
