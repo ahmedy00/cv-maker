08dc2e204faa58d82322b034688a0dc3a3c443b8
Ahmet <dede_dede_ahmet@hotmail.com>
2023-08-03 03:28:46 +0300

    UI changes for header

--- src/components/home/HomeHeader.vue
+++ src/components/home/HomeHeader.vue
@@ -12,64 +12,32 @@
             <span class="item mr-6">{{ $t('PreviousResumes') }}</span>
           </RouterLink>
           <span class="item item-link cursor-pointer mr-6">Lorem</span>
-        </v-col>
-        <v-col class="d-flex align-center justify-content-end pr-16" :cols="4">
           <v-menu>
             <template v-slot:activator="{ props }">
-                <v-icon :color="'white'" :size="18" v-bind="props">
+                <v-icon :color="'white'" :size="20" v-bind="props">
                   <i class="fa-solid fa-earth-asia"></i>
                 </v-icon>
             </template>
-           <v-card>
-            <v-list class="pa-0">
-              <v-list-item
-                class="pa-0"
-                v-for="(language, index) in languages"
-                :key="`language-${index}`"
-              >
-                <v-btn
-                  @click="changeLanguage(language)"
-                  dense
+            <v-card>
+              <v-list class="pa-0">
+                <v-list-item
+                  class="pa-0"
+                  v-for="(language, index) in languages"
+                  :key="`language-${index}`"
                 >
-                  {{ language.name }}
-                </v-btn>
-              </v-list-item>
-            </v-list>
-          </v-card>
-              </v-menu>
+                  <v-btn
+                    @click="changeLanguage(language)"
+                    dense
+                  >
+                    {{ language.name }}
+                  </v-btn>
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </v-menu>
         </v-col>
       </v-row>
     </v-container>
-  <div>
-    <header :style="{height: `${props.headerHeight}px`, background: 'red'}">
-      <v-container fluid="">
-        <v-row>
-          <v-col @click="routeToHome">
-            <img width="45" src="https://s3-alpha.figma.com/hub/file/1913095808/a7bdc469-cd70-4ea1-bb57-b59204ad8182-cover.png" alt="">
-          </v-col>
-        </v-row>
-      </v-container>
-        <div class="container-fluid h-100">
-          <div class="row w-100 h-100">
-            <div class="col-5 ms-5 d-flex align-items-center cursor-pointer" @click="routeToHome">
-              <img width="45" src="https://s3-alpha.figma.com/hub/file/1913095808/a7bdc469-cd70-4ea1-bb57-b59204ad8182-cover.png" alt="">
-            </div>
-            <div class="item-container col-6 d-flex flex-row-reverse align-items-center">
-              <span class="item item-link cursor-pointer me-4">Lorem</span>
-              <RouterLink class="item-link" to="">
-                <span class="item me-4">{{ $t('PreviousResumes') }}</span>
-              </RouterLink>
-              <span class="me-4">
-                <a class="item-link item" href="https://github.com/ahmedy00/cv-maker" target="_blank">{{ $t('Contribution') }}</a>
-              </span>
-            </div>
-            <div class="col-1">
-
-            </div>
-          </div>
-        </div>
-    </header>
-  </div>
 </template>
 
 <script setup lang="ts">
